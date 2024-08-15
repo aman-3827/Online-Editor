fcb4a12b9753156182d571c58e722ca0f8a3a3ce
Aman kumar <kumaramanddn390@gmail.com>
2024-08-16 00:05:14 +0530

    React

--- JS/Closures/script.js
+++ JS/Closures/script.js
@@ -1,3 +1,19 @@
-function adder(num){
+const myname = document.getElementById("my-name");
+const btn = document.getElementById("mybutton");
 
+function maketextsizer(size){
+    function changesize(){
+        myname.style.fontSize = `${size}px`;
+    }
+
+    return changesize;
 }
+
+const size12 = maketextsizer(12);
+const size14 = maketextsizer(14);
+const size16 = maketextsizer(16);
+const size18 = maketextsizer(18);
+const size20 = maketextsizer(20);
+
+
+btn.addEventListener('click',size20 )
